d7cf8119212e1b18ad16d0440f8105950e60cbda
domgolonka <1701220+domgolonka@users.noreply.github.com>
2025-04-10 06:55:31 -0700

    updated log

--- conn_pool.go
+++ conn_pool.go
@@ -1,6 +1,6 @@
 package sharding
 
-import x``(
+import (
 	"context"
 	"database/sql"
 	"errors"
@@ -11,36 +11,6 @@ import x``(
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
-
-type LogLevel int
-
-const (
-	LogLevelError LogLevel = iota
-	LogLevelInfo
-	LogLevelDebug
-	LogLevelTrace
-)
-
-var (
-	DefaultLogLevel = LogLevelInfo
-	ErrNotSupported = errors.New("operation not supported by underlying connection pool")
-)
-
-func debugLog(format string, args ...interface{}) {
-	GetLogger().Debug(format, args...)
-}
-
-func traceLog(format string, args ...interface{}) {
-	GetLogger().Trace(format, args...)
-}
-
-func infoLog(format string, args ...interface{}) {
-	GetLogger().Info(format, args...)
-}
-
-func errorLog(format string, args ...interface{}) {
-	GetLogger().Error(format, args...)
-}
 
 // ConnPool wraps standard GORM ConnPool to handle sharding transparently for client code
 type ConnPool struct {
